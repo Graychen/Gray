9adebdde4a03c686e9854581f66b996918cd7c05
chenjiahui <15988358525@163.com>
2013-10-24 17:31:19 +0800

    1314显示防盗

---
+++ Security/Security/src/com/xiaobin/security/receiver/CallPhoneReceiver.java
@@ -0,0 +1,24 @@
+package com.xiaobin.security.receiver;
+
+import com.xiaobin.security.ui.LostProtectedActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CallPhoneReceiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		String outPhoneNumber = this.getResultData();
+		if(outPhoneNumber.equals("1214"))
+		{
+			Intent i = new Intent(context, LostProtectedActivity.class);
+			i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+			context.startActivity(i);
+			setResultData(null);
+		}
+	}
+
+}
